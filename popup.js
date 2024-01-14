e0273d7afdb224b05507232f1654e86464c8a060
Shourya Chaudhry <149008800+18aaddy@users.noreply.github.com>
2024-01-14 09:22:53 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,14 @@
+
+    // Get the reference to the div
+    var myDiv = document.getElementsByClassName("dialogue");
+
+    // Check the condition
+    if (nftTouch) {
+        // Show the div
+        myDiv.style.display = "block";
+    } else {
+        // Hide the div
+        myDiv.style.display = "none";
+    }
+
+// import {player} from "./index";
